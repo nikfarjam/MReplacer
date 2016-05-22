50b27eddc0f2f69d2030c8c8b091f64aa760fd45
nikfarjam <mm.nikfarjam@gmail.com>
2016-05-22 18:36:35 +1000

    add string util

---
+++ src/main/java/au/com/mehdi/replacer/util/StringUtil.java
@@ -0,0 +1,22 @@
+package au.com.mehdi.replacer.util;
+
+/**
+ * Util class for String operations
+ */
+public interface StringUtil {
+
+    /**
+     * check if a string is null or empty
+     * @param str string value which is checked
+     * @return true, when the input is null or ""
+     */
+    static boolean isEmpty(String str) {
+        if (str == null) {
+            return true;
+        }
+        if (str.length() == 0) {
+            return true;
+        }
+        return str.replaceAll(" ", "").length() == 0;
+    }
+}
