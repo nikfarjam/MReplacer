015626e81df7cef5e411c37475c9f6ca5f0a42a7
nikfarjam <mm.nikfarjam@gmail.com>
2016-05-29 20:12:38 +1000

    Added interface for Input Form

---
+++ src/main/java/au/com/mehdi/replacer/ui/InputForm.java
@@ -0,0 +1,11 @@
+package au.com.mehdi.replacer.ui;
+
+import au.com.mehdi.replacer.data.ReplaceData;
+
+/**
+ * Created by mehdi on 5/29/16.
+ */
+public interface InputForm {
+
+    ReplaceData getData();
+}
