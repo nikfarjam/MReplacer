26104a4dd713761062f548d4a48be8d5be637f1d
nikfarjam <mm.nikfarjam@gmail.com>
2016-05-29 20:19:38 +1000

    Added a factory to create UI components

---
+++ src/main/java/au/com/mehdi/replacer/util/UIFactory.java
@@ -0,0 +1,51 @@
+package au.com.mehdi.replacer.util;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+/**
+ * Factory for UI components
+ */
+public class UIFactory {
+
+    private UIFactory() {
+    }
+
+    public static JLabel createLabel(String key) {
+        String text = ConfigurationUtil.getInstance().getValue(key);
+        if (StringUtil.isEmpty(text)) {
+            text = "Unknown";
+        }
+        JLabel label = new JLabel(text);
+        return label;
+    }
+
+    public static JButton createButton(String key, List<ActionListener> listeners) {
+        String text = ConfigurationUtil.getInstance().getValue(key);
+        if (StringUtil.isEmpty(text)) {
+            text = "Unknown";
+        }
+        JButton button = new JButton(text);
+        if (listeners != null) {
+            listeners.forEach(l -> button.addActionListener(l));
+        }
+        return button;
+    }
+
+    public static JButton createButton(String key, ActionListener... listeners) {
+        String text = ConfigurationUtil.getInstance().getValue(key);
+        if (StringUtil.isEmpty(text)) {
+            text = "Unknown";
+        }
+        JButton button = new JButton(text);
+        if (listeners != null) {
+            for (ActionListener l : listeners) {
+                if (l != null) {
+                    button.addActionListener(l);
+                }
+            }
+        }
+        return button;
+    }
+}
