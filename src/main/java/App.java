2e88a12e8a71567bbdf4545bbd46f5cf8f007c2a
nikfarjam <mm.nikfarjam@gmail.com>
2016-06-05 16:35:56 +1000

    added log to main app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import au.com.mehdi.replacer.ui.ReplacerMainFrame;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
 
@@ -6,11 +8,13 @@ import javax.swing.*;
  * Main class of application.
  */
 public class App {
+    private static final Logger logger = LoggerFactory.getLogger(App.class);
 
     private App() {
     }
 
     public static void main(String[] args) {
+        logger.warn("Start Multi replacer");
         SwingUtilities.invokeLater(() -> {
             new ReplacerMainFrame();
         });
