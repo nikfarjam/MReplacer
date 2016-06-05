d6fcacc667f05fec08d7ec700d0f26eac3a07095
nikfarjam <mm.nikfarjam@gmail.com>
2016-06-05 16:37:08 +1000

    add search label

--- src/main/java/au/com/mehdi/replacer/util/LabelConstants.java
+++ src/main/java/au/com/mehdi/replacer/util/LabelConstants.java
@@ -8,6 +8,7 @@ public class LabelConstants {
     public static final String MAIN_TITLE = "ui.main.title";
     public static final String BUTTON_OPEN = "ui.main.button.open";
     public static final String BUTTON_REPLACE = "ui.main.button.replace";
+    public static final String BUTTON_SEARCH = "ui.main.button.search";
     public static final String LABEL_PATH = "ui.main.label.path";
     public static final String REGULAR_EXPRESSION = "ui.main.label.regular_expression";
     public static final String LABEL_PATTERN = "ui.main.label.pattern";
