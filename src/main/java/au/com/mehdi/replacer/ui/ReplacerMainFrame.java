03ab769c496405c8f0938b511eb675c08d13cfe5
nikfarjam <mm.nikfarjam@gmail.com>
2016-06-05 16:41:16 +1000

    remove search button

--- src/main/java/au/com/mehdi/replacer/ui/ReplacerMainFrame.java
+++ src/main/java/au/com/mehdi/replacer/ui/ReplacerMainFrame.java
@@ -2,6 +2,7 @@ package au.com.mehdi.replacer.ui;
 
 import au.com.mehdi.replacer.util.ConfigurationUtil;
 import au.com.mehdi.replacer.util.LabelConstants;
+import au.com.mehdi.replacer.util.UIFactory;
 
 import javax.swing.*;
 import java.awt.*;
@@ -15,7 +16,6 @@ public class ReplacerMainFrame extends JFrame implements ActionListener {
 
     private InputPanel inputPanel;
     private JTable jtParam;
-    private JButton jbSearch;
     private JButton jbReplace;
 
     public ReplacerMainFrame() throws HeadlessException {
@@ -27,9 +27,12 @@ public class ReplacerMainFrame extends JFrame implements ActionListener {
         inputPanel = new InputPanel();
         panel.add(inputPanel, BorderLayout.CENTER);
 
-        add(panel, BorderLayout.CENTER);
+        jbReplace = UIFactory.createButton(LabelConstants.BUTTON_SEARCH);
 
-        setSize(420, 150);
+        add(panel, BorderLayout.CENTER);
+        add(jbReplace, BorderLayout.AFTER_LAST_LINE);
+
+        setSize(420, 180);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setVisible(true);
     }
