e96adbedab53a3f75d231a51639625b63174c915
nikfarjam <mm.nikfarjam@gmail.com>
2016-06-25 23:27:22 +1000

    new test method for file chooser behavior

--- src/test/java/au/com/mehdi/replacer/gui/ReplacerMainFrameTest.java
+++ src/test/java/au/com/mehdi/replacer/gui/ReplacerMainFrameTest.java
@@ -1,9 +1,12 @@
 package au.com.mehdi.replacer.gui;
 
+import org.fest.swing.finder.JFileChooserFinder;
 import org.fest.swing.fixture.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.File;
 
 import static au.com.mehdi.replacer.util.LabelConstants.*;
 
@@ -42,6 +45,39 @@ public class ReplacerMainFrameTest {
         getButtonCancel().requireDisabled();
 
         getTable().requireDisabled();
+    }
+
+    @Test
+    public void disableReplaceButton() throws Exception {
+        getTxtPattern().setText("$param");
+        getButtonSearch().requireDisabled();
+        getButtonCancel().requireDisabled();
+
+        getCheckRegExpr().check();
+        getButtonSearch().requireDisabled();
+        getButtonCancel().requireDisabled();
+    }
+
+    @Test
+    public void selectFile() throws Exception {
+        //given
+        File file = getFile();
+        getTxtPattern().setText("$param");
+        getButtonOpen().click();
+
+        JFileChooserFixture fileChooser = JFileChooserFinder.findFileChooser().using(demo.robot);
+        fileChooser.setCurrentDirectory(file.getParentFile());
+        fileChooser.selectFile(file.getAbsoluteFile());
+        fileChooser.approve();
+
+//        then
+        getTxtPath().requireText(file.getAbsolutePath());
+        getButtonSearch().requireEnabled();
+        getButtonCancel().requireDisabled();
+    }
+
+    private File getFile() {
+        return new File("src/test/resources/basic_test.txt");
     }
 
     private JTextComponentFixture getTxtPattern() {
