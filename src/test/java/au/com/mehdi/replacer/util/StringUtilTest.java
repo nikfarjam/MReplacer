74aa74812abe74abb1eaace9b603871ba36037f2
nikfarjam <mm.nikfarjam@gmail.com>
2016-07-17 13:24:37 +1000

    test: add test methods to verify isEmpty method

--- src/test/java/au/com/mehdi/replacer/util/StringUtilTest.java
+++ src/test/java/au/com/mehdi/replacer/util/StringUtilTest.java
@@ -1,10 +1,29 @@
 package au.com.mehdi.replacer.util;
 
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
- * Created by mehdi on 7/17/16.
+ * Unit test class for String Util
  */
 public class StringUtilTest {
 
+    @Test
+    public void isEmpty() throws Exception {
+        assertTrue(StringUtil.isEmpty(null));
+        assertTrue(StringUtil.isEmpty(""));
+        assertTrue(StringUtil.isEmpty("   "));
+
+        assertFalse(StringUtil.isEmpty("1"));
+        assertFalse(StringUtil.isEmpty(" 1 "));
+    }
+
+    @Test
+    public void noChangeAfterCallIsEmpty() {
+        String sample = "    ABC  ";
+        String origin = String.valueOf(sample);
+        StringUtil.isEmpty(sample);
+        assertEquals(sample, origin);
+    }
 }
