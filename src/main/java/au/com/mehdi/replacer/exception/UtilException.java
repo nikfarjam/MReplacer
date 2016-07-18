60b8866926e94ea84d5a112204754f46baf5c585
nikfarjam <mm.nikfarjam@gmail.com>
2016-07-18 19:00:25 +1000

    new: add exception for utilities

---
+++ src/main/java/au/com/mehdi/replacer/exception/UtilException.java
@@ -0,0 +1,11 @@
+package au.com.mehdi.replacer.exception;
+
+/**
+ * Exception in utility
+ */
+public class UtilException extends Exception {
+
+    public UtilException(String message) {
+        super(message);
+    }
+}
