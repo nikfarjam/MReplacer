56ef48ef405e19985c91194e5ff2bf8e8b70534c
nikfarjam <mm.nikfarjam@gmail.com>
2016-07-18 20:46:56 +1000

    test: add test class for file util

---
+++ src/test/java/au/com/mehdi/replacer/util/FileUtilTest.java
@@ -0,0 +1,22 @@
+package au.com.mehdi.replacer.util;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by mehdi on 7/18/16.
+ */
+public class FileUtilTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
